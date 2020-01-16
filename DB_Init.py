36ea1c9ad8553bfb2d316fcd8420460d3d327ba6
sxw455 <Stan.Ward@Sherwin.com>
2020-01-16 16:46:50 -0500

    Commit 2020-01-16 16:46 -

--- DB_Init.py
+++ DB_Init.py
@@ -4,10 +4,31 @@ db.drop_all()
 db.create_all()
 db.session.commit()
 
-new=User('stan','pass1')
+# Add users
+new=User('Stan','Ward')
 db.session.add(new)
 
 new=User('Fred','Flintstone')
+db.session.add(new)
+
+db.session.commit()
+
+# Add Climates
+new=Climate('subarctic')
+db.session.add(new)
+
+new=Climate('arctic')
+db.session.add(new)
+
+db.session.commit()
+
+# Add Items
+new=Item('shirt',1)
+#,'viking','subartic','male','clothing',1500,1599,'Its a shirt')
+db.session.add(new)
+
+new = Item('pants',1)
+#,'viking','subartic','male','clothing',1500,1599,"Pants!")
 db.session.add(new)
 
 db.session.commit()
